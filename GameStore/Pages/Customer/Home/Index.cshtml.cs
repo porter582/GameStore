9167f9e2780dd66b850595fe2b9e9e416199b463
porter582 <porterokey@mail.weber.edu>
2020-02-12 20:47:01 -0700

    Cards on main index page, VM done

--- GameStore/Pages/Customer/Home/Index.cshtml.cs
+++ GameStore/Pages/Customer/Home/Index.cshtml.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Game.Models;
+using GameStore.DataAccess;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GameStore.Pages.Customer.Home
 {
     public class IndexModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IEnumerable<GameModel> GameList { get; set; }
+        public IEnumerable<Game.Models.Genre> GenreList { get; set; }
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public void OnGet()
         {
-
+            GameList = _unitOfWork.GameObj.GetAll(null, null, "Genre,Rating");
+            GenreList = _unitOfWork.Genre.GetAll(null, null, null);
         }
     }
 }
