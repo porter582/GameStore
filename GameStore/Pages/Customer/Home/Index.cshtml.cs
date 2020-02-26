26264e8a5478a19c4ca8e3f106060308eb64d18a
porter582 <porterokey@mail.weber.edu>
2020-02-25 20:13:47 -0700

    Add option to add to cart from index, and change up style

--- GameStore/Pages/Customer/Home/Index.cshtml.cs
+++ GameStore/Pages/Customer/Home/Index.cshtml.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using Game.Models;
+using Game.Utility;
 using GameStore.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GameStore.Pages.Customer.Home
@@ -9,6 +13,9 @@ namespace GameStore.Pages.Customer.Home
     public class IndexModel : PageModel
     {
         private readonly IUnitOfWork _unitOfWork;
+
+        [BindProperty]
+        public ShoppingCart ShoppingCartObj { get; set; }
 
         public IEnumerable<GameModel> GameList { get; set; }
         public IEnumerable<Game.Models.Genre> GenreList { get; set; }
@@ -21,6 +28,55 @@ namespace GameStore.Pages.Customer.Home
         {
             GameList = _unitOfWork.GameObj.GetAll(null, null, "Genre,Rating");
             GenreList = _unitOfWork.Genre.GetAll(null, null, null);
+
+        }
+
+        public IActionResult OnPost(int id)
+        {
+
+            ShoppingCartObj = new ShoppingCart()
+            {
+                GameItem = _unitOfWork.GameObj.GetFirstOrDefault
+                (includeProperties: "Genre,Rating", filter: c => c.Id == id),
+                GameItemId = id
+            };
+
+            if (ModelState.IsValid && User.IsInRole(SD.CustomerRole)) //change later
+            {
+                var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+                ShoppingCartObj.ApplicationUserId = claim.Value;
+                ShoppingCart cartFromDb =
+                    _unitOfWork.ShoppingCart.GetFirstOrDefault(c =>
+                    c.ApplicationUserId == ShoppingCartObj.ApplicationUserId &&
+                    c.GameItemId == ShoppingCartObj.GameItemId);
+
+                //does a shopping cart (item list) exist in the db
+                if (cartFromDb == null)
+                {
+                    _unitOfWork.ShoppingCart.Add(ShoppingCartObj);
+                }
+
+                //already exists for this item/user combo just update count
+                else
+                {
+                    _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, ShoppingCartObj.Count);
+                }
+
+                _unitOfWork.Save();
+                var count = _unitOfWork.ShoppingCart.GetAll(c =>
+                c.ApplicationUserId == ShoppingCartObj.ApplicationUserId).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ShoppingCart, count);
+                return RedirectToPage("Index");
+            }
+            else //Adding new item
+            {
+                ShoppingCartObj.GameItem =
+                    _unitOfWork.GameObj.GetFirstOrDefault(includeProperties:
+                    "Genre,Rating", filter: c => c.Id == ShoppingCartObj.GameItemId);
+                return Page();
+            }
         }
     }
 }
