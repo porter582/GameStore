ba1c5423873f3b949e9ffd71f855abd3c900a58f
porter582 <porterokey@mail.weber.edu>
2020-02-25 19:33:35 -0700

    02/25/2020

--- GameStore/Pages/Customer/Cart/Index.cshtml.cs
+++ GameStore/Pages/Customer/Cart/Index.cshtml.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using Game.Models.ViewModels;
+using Game.Utility;
+using GameStore.DataAccess;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +12,88 @@ namespace GameStore.Pages.Customer.Cart
 {
     public class IndexModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public OrderDetailsCart OrderDetailsCartVM { get; set; }
         public void OnGet()
         {
+            OrderDetailsCartVM = new OrderDetailsCart()
+            {
+                OrderHeader = new Game.Models.OrderHeader(),
+                listCart = new List<Game.Models.ShoppingCart>()
+            };
 
+            OrderDetailsCartVM.OrderHeader.OrderTotal = 0.0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                IEnumerable<Game.Models.ShoppingCart> cart =
+                    _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value);
+
+                if (cart != null)
+                {
+                    OrderDetailsCartVM.listCart = cart.ToList();
+                }
+
+                foreach (var cartList in OrderDetailsCartVM.listCart)
+                {
+                    cartList.GameItem = _unitOfWork.GameObj.GetFirstOrDefault
+                        (m => m.Id == cartList.GameItemId);
+                    OrderDetailsCartVM.OrderHeader.OrderTotal +=
+                        (cartList.GameItem.Price * cartList.Count);
+                }
+            }
+        }
+
+        public IActionResult OnPostPlus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault
+                (c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostMinus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault
+                (c => c.Id == cartId);
+            if (cart.Count == 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+            }
+            _unitOfWork.Save();
+            var cnt = _unitOfWork.ShoppingCart.GetAll
+                (u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+
+            HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostRemove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault
+                (c => c.Id == cartId);
+
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            var cnt = _unitOfWork.ShoppingCart.GetAll
+                (u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+
+            HttpContext.Session.SetInt32(SD.ShoppingCart, cnt);
+            return RedirectToPage("/Customer/Cart/Index");
         }
     }
 }
---
+++ GameStore/Pages/Customer/Home/Details.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Game.Models;
+using Game.Utility;
+using GameStore.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GameStore.Pages.Customer.Home
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DetailsModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public ShoppingCart ShoppingCartObj { get; set; }
+
+        public void OnGet(int id)
+        {
+            ShoppingCartObj = new ShoppingCart()
+            {
+                GameItem = _unitOfWork.GameObj.GetFirstOrDefault
+                (includeProperties: "Genre,Rating", filter: c => c.Id == id),
+                GameItemId = id
+            };
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid && User.IsInRole(SD.CustomerRole)) //change later
+            {
+                var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+                ShoppingCartObj.ApplicationUserId = claim.Value;
+                ShoppingCart cartFromDb =
+                    _unitOfWork.ShoppingCart.GetFirstOrDefault(c =>
+                    c.ApplicationUserId == ShoppingCartObj.ApplicationUserId &&
+                    c.GameItemId == ShoppingCartObj.GameItemId);
+
+                //does a shopping cart (item list) exist in the db
+                if (cartFromDb == null)
+                {
+                    _unitOfWork.ShoppingCart.Add(ShoppingCartObj);
+                }
+
+                //already exists for this item/user combo just update count
+                else
+                {
+                    _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, ShoppingCartObj.Count);
+                }
+
+                _unitOfWork.Save();
+                var count = _unitOfWork.ShoppingCart.GetAll(c =>
+                c.ApplicationUserId == ShoppingCartObj.ApplicationUserId).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ShoppingCart, count);
+                return RedirectToPage("Index");
+            }
+            else //Adding new item
+            {
+                ShoppingCartObj.GameItem =
+                    _unitOfWork.GameObj.GetFirstOrDefault(includeProperties:
+                    "Genre,Rating", filter: c => c.Id == ShoppingCartObj.GameItemId);
+                return Page();
+            }
+        }
+    }
+}
